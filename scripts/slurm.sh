26d84474fd297e561b88007ed6dafc707aa57970
William Hardy <wilah@ulaval.ca>
2025-09-22 17:15:37 -0400

    wip slurm

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -5,7 +5,13 @@
 #SBATCH --job-name=eepp_train
 #SBATCH --output=slurm_output/%x-%j.out
 
+# Create a temporary file to store the key securely
 temp_file=$(mktemp)
+
+# Ensure the temporary file is deleted when the script exits
+trap 'rm -f "$temp_file"' EXIT
+
+# Write the key to the temporary file
 echo "$WANDB_API_KEY" > "$temp_file"
 
 apptainer exec --nv --bind /datasets/eepp:/datasets --bind /etc/pki:/etc/pki --bind "$temp_file:/run/wandb_api_key.txt" ./scripts/train_token_dinov2_EEPP.sif bash \
