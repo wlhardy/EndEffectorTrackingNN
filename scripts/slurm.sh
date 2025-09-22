05a5d863d8a9ca91a5fadae2ff322a17ce8a1083
William Hardy <wilah@ulaval.ca>
2025-09-22 16:51:15 -0400

    wip slurm

--- scripts/slurm.sh
+++ scripts/slurm.sh
@@ -3,6 +3,6 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --time=7-00
 #SBATCH --job-name=eepp_train
-#SBATCH --output=%x-%j.out
+#SBATCH --output=slurm_output/%x-%j.out
 
 apptainer exec --nv --bind /datasets/eepp:/datasets --bind /etc/pki:/etc/pki ./scripts/train_token_dinov2_EEPP.sif bash
